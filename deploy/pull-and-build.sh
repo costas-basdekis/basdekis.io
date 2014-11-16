58a07af7127c600c2c7a8192867d16ba5ab9f82e
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-16 11:12:15 +0000

    Fix git stash command in pull script

--- deploy/pull-and-build.sh
+++ deploy/pull-and-build.sh
@@ -5,11 +5,13 @@ set -e
 BASEDIR=$(dirname $0)
 
 date
+echo $(who)
 
 cd $BASEDIR
 
+echo 'stashing'
 git add -A
-git stash "STASH TO PULL AND BUILD" || true
+git stash save "STASH TO PULL AND BUILD" || true
 
 git checkout build
 echo 'checked out build'

d83cd42e057595c0616cce115376df2a0552a968
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-24 02:47:17 +0000

    Remove accidental i - damn vim

--- deploy/build.sh
+++ deploy/build.sh
@@ -13,7 +13,7 @@ cp deploy/nginx/basdekis.io.conf /etc/nginx/sites-available/
 echo 'put webhook url'
 github_webhook_url=$(cat ../github-webhook-url)
 sed -i "s/{github-webhook-url}/"$github_webhook_url"/" /etc/nginx/sites-available/basdekis.io.conf
-ln -sf /etc/nginx/sites-available/basdekis.io.confi /etc/nginx/sites-enabled/basdekis.io.conf
+ln -sf /etc/nginx/sites-available/basdekis.io.conf /etc/nginx/sites-enabled/basdekis.io.conf
 echo 'reload nginx'
 nginx -t
 service nginx reload
